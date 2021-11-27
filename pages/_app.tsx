3494a4f3b33607a48ce16d37822e2bc2bd5146f8
Aleksandr Chuprynskyi <chuprynskyi_ak16@nuwm.edu.ua>
2021-11-27 22:44:36 +0200

    129

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,12 +11,16 @@ function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="preconnect" href="https://mc.yandex.ru" />
         <link
           href="https://fonts.googleapis.com/css2?family=Manrope:wght@300;400;500;700&display=swap"
           rel="stylesheet"
         />
-        <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
-        <meta property="og:locale" content='ru_RU' />
+        <meta
+          property="og:url"
+          content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}
+        />
+        <meta property="og:locale" content="ru_RU" />
       </Head>
       <Component {...pageProps} />
     </>
